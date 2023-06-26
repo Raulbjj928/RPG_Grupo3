4d529865289ab7e03f8f12a36a5a7f3893c895f1
Raul <raul.rhf9316@gmail.com>
2023-06-26 11:38:09 -0300

    Renomeando pastas

--- DungeonsAndDevs/DungeonsAndDevs/Entities/Characters/Enemies/Enemy.cs
+++ DungeonsAndDevs/DungeonsAndDevs/Entities/Characters/Enemies/Enemy.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DungeonsAndDevs.Entidades.Personagens.Inimigos
+namespace DungeonsAndDevs.Entities.Personagens.Inimigos
 {
 	public class Enemy : Character
 	{
