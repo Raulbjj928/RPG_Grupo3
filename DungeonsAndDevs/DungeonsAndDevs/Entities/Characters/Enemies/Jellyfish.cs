6a2e0ecca22314017d97c7ed2c8511a81fa28497
Gustavo <g.hamann93@gmail.com>
2023-06-26 22:38:17 -0300

    Criando classes de inimigos

---
+++ DungeonsAndDevs/DungeonsAndDevs/Entities/Characters/Enemies/Jellyfish.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDevs.Entities.Characters.Enemies
+{
+    internal class Jellyfish : Enemy
+    {
+        public Jellyfish() 
+        {
+            Name = "jellyfish";
+            EnemyType = "Monster";
+            EnemyLevel = 3;
+            Health = 98;
+            Strength = 15;
+            Defense = 15;
+            Skills.Add(new Utils.Skill("Tiro de Concussão", DamageType.Eletric, 5, 0, false));
+            Skills.Add(new Utils.Skill("Tiro Penetrante", DamageType.Fire, 5, 0, false));
+            Skills.Add(new Utils.Skill("Tiro Inflamável", DamageType.Poison, 5, 0, false));
+            Advantages.Add(DamageType.Eletric);
+            Disadvantages.Add(DamageType.Cut);
+            EnemyBaseXP = 6;
+        }
+    }
+}
---
+++ DungeonsAndDevs/DungeonsAndDevs/Entities/Characters/Enemies/Shark.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDevs.Entities.Characters.Enemies
+{
+    internal class Shark : Enemy
+    {
+        public Shark() 
+        {
+            Name = "White Shark";
+            EnemyType = "Monster";
+            EnemyLevel = 1;
+            Health = 105;
+            Strength = 15;
+            Defense = 8;
+            Skills.Add(new Utils.Skill("Fúria Aquática", DamageType.Impact, 5, 0, false));
+            Skills.Add(new Utils.Skill("Presas Implacáveis", DamageType.Cut, 5, 0, false));
+            Skills.Add(new Utils.Skill("Espiral do Terror", DamageType.Cut, 5, 0, false));
+            Disadvantages.Add(DamageType.Impact);
+            EnemyBaseXP = 1;
+        }  
+    }
+}
